46e4cea20b8ad620f6a7ce73d30c95b4c0fe8395
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-04-13 20:42:21 +0530

    added cors and some other stuff

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
-import express from 'express'
-import commentHandler from './commentHandler.js'
+import express from "express";
+import commentHandler from "./commentHandler.js";
+import cors from "cors";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+	})
+);
 
-app.route('/comments')
-    .post(commentHandler)
-    .get(() => console.log("This is working!"))
+app.route("/comments")
+	.post(commentHandler)
+	.get((req, res) => {
+		res.status(200).json({
+			message: "You are doing great!",
+		});
+	});
 
 export default app;
--- server/commentHandler.js
+++ server/commentHandler.js
@@ -3,9 +3,7 @@ import sentimentClassifier from "./commentsModel.js";
 const predictComment = async (req, res) => {
 	try {
 		const answer = await sentimentClassifier.find(req.body);
-		// console.log(req.body);
 		res.status(200).json({ answer: answer });
-		// console.log(answer);
 	} catch (err) {
 		console.log(err);
 		res.status(500).json({ message: err });
